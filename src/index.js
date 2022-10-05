5693ebd4e6bfe01a8ee1ef01b531a56ffad64e46
Oleksandr Tsabak <sasha.tsabak@gmail.com>
2022-10-05 13:02:35 +0300

    fuxed parcel handlebars

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './css/styles.css';
+// import './css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 import countryList from './templates/country-list.hbs';
